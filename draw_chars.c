85c09ff7ff43b1f48f6b45bd83ee45bb7ff1c3de
Ximena Parada <xphere24@gmail.com>
2022-01-31 17:01:43 -0700

    Worked, now problem with font .o files

--- draw_chars.c
+++ draw_chars.c
@@ -13,3 +13,23 @@ void print_char_11x16(char c)
     putchar('\n');
   }
 }
+/* 
+   expr1 ? expr2 : expr3
+   expr1 is evaluated first, if it is non-zero (true), then, expr 2 is evaluated
+   else, expr3 will be evaluated
+ */
+
+// My work...
+
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 8; row++) {
+    unsigned short rowBits = font_8x12[c][row];
+    for (char col = 0; col < 12; col++) {
+      unsigned short colMask = 1 << (11-col); /* mask to select bit associated with bit */
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+} 
